285f9edc181c12883f77c0117af8525f9774aa09
FTD <>
2022-03-26 23:04:35 +0900

    [hotfix] change publicPath to addresses an issue where pages below "work" cannot be displayed

--- vue.config.js
+++ vue.config.js
@@ -86,5 +86,7 @@ module.exports = defineConfig({
   // set path for github pages
   outputDir: 'docs',
   assetsDir: './',
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/production-sub-path/'
+    : '/',
 })
