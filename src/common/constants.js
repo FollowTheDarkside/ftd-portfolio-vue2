e875f97e96a275e88eb39f736c125bec34b248b8
FTD <>
2022-03-26 22:26:48 +0900

    [update] change url for github pages

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,55 +2,55 @@ export default {
   menuItems: [
     {
       name: 'TOP',
-      url: 'http://192.168.0.6:8080'
+      url: 'https://followthedarkside.github.io/'
     },
     {
       name: 'INFO',
-      url: 'http://192.168.0.6:8080/info.html'
+      url: 'https://followthedarkside.github.io/info.html'
     },
     {
       name: 'OTHER',
-      url: 'http://192.168.0.6:8080/other.html'
+      url: 'https://followthedarkside.github.io/other.html'
     },
     {
       name: 'CONTACT',
-      url: 'http://192.168.0.6:8080/contact.html'
+      url: 'https://followthedarkside.github.io/contact.html'
     },
   ],
   workImages: [
     {
       src: require("../assets/work/work7.png"),
-      url: "http://192.168.0.6:8080/work/gan-sanpo-ikuta.html",
+      url: "https://followthedarkside.github.io/work/gan-sanpo-ikuta.html",
       gradient_over: "to top right, rgba(255,255,255,.0), rgba(255,255,255,.0)",
     },
     {
       src: require("../assets/work/work6.png"),
-      url: "http://192.168.0.6:8080/work/the-freshest-ai.html",
+      url: "https://followthedarkside.github.io/work/the-freshest-ai.html",
       gradient_over: "to top right, rgba(255,255,255,.0), rgba(255,255,255,.0)",
     },
     {
       src: require("../assets/work/work5.png"),
-      url: "http://192.168.0.6:8080/work/breakgan-ft-steez.html",
+      url: "https://followthedarkside.github.io/work/breakgan-ft-steez.html",
       gradient_over: "to top right, rgba(255,255,255,.0), rgba(255,255,255,.0)",
     },
     {
       src: require("@/assets/work/work4.png"),
-      url: "http://192.168.0.6:8080/work/breakgan.html",
+      url: "https://followthedarkside.github.io/work/breakgan.html",
       gradient_over: "to top right, rgba(255,255,255,.0), rgba(255,255,255,.0)",
     },
     {
       src: require("@/assets/work/work3.png"),
-      url: "http://192.168.0.6:8080/work/tokaido53-hiroshige-effect.html",
+      url: "https://followthedarkside.github.io/work/tokaido53-hiroshige-effect.html",
       gradient_over: "to top right, rgba(255,255,255,.0), rgba(255,255,255,.0)",
     },
     {
       src: require("@/assets/work/work2.png"),
-      url: "http://192.168.0.6:8080/work/danceai-performance-at-stm32-summit.html",
+      url: "https://followthedarkside.github.io/work/danceai-performance-at-stm32-summit.html",
       gradient_over: "to top right, rgba(255,255,255,.0), rgba(255,255,255,.0)",
     },
     {
       src: require("@/assets/work/work1.png"),
-      url: "http://192.168.0.6:8080/work/dance-ai-for-beginners.html",
+      url: "https://followthedarkside.github.io/work/dance-ai-for-beginners.html",
       gradient_over: "to top right, rgba(255,255,255,.0), rgba(255,255,255,.0)",
     },
   ]
